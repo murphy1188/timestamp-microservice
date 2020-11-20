adde2bf5eecf01a0fb3c680656e0256531ca5f5b
murphy1188 <64935737+murphy1188@users.noreply.github.com>
2020-11-20 12:15:20 -0700

    Upload repo from repl
    Upload timestamp microservice repo from repl

---
+++ server.js
@@ -0,0 +1,49 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
+});
+
+app.get("/api/timestamp/:date", (req, res) => {
+  dateInput = req.params.date;
+  
+  if (/\d{5,}/.test(dateInput)){
+    res.json({ unix: parseInt(dateInput), utc: new Date(parseInt(dateInput)).toUTCString() });
+  };
+
+  let dateObj = new Date(dateInput);
+  if (dateObj.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  }
+  res.json({ unix: dateObj.valueOf(), utc: dateObj.toUTCString() });
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
